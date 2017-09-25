1c11a9140c9e932288afe95626ed3b71575d5341
Christian Bryant <christian.bryant@mobelux.com>
2017-09-25 17:37:14 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+
+// Force https in production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(function(req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(['https://', req.get('Host'), req.url].join(''));
+        }
+        return next();
+    });
+  }
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/static/css",express.static(__dirname + "/static/css"));
+app.use("/static/scss",express.static(__dirname + "/static/scss"));
+app.use("/static/js",express.static(__dirname + "/static/js"));
+app.use("/static/img",express.static(__dirname + "/static/img"));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+app.listen(process.env.PORT || 8081, function () {
+  console.log('Listening on port 8081!')
+})
---
+++ static/js/main.js
@@ -0,0 +1,40 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.addEventListener("mousewheel", handleScroll);
+  document.addEventListener("DOMMouseScroll", handleScroll);
+
+
+  function handleScroll() {
+
+    // NOTE: Based off the active section height, check to see where
+    //       in that section they are currnetly in their scroll.
+    //       Then use that as a timeline to show 'x' percent of whatevever shape / slide
+    //       needs to be animated.
+
+    const currentScrollPosition = window.scrollY;
+    const currentSection = document.getElementsByClassName("homepage")[0].getAttribute("data-active-section");
+    const currentSectionContent = document.getElementsByClassName("section-two__content")[0]
+
+    const sectionScrollHeight = document.getElementsByClassName(`section-${currentSection}`)[0].scrollHeight;
+    let sectionScrolledPercentX = Math.round( (currentScrollPosition / sectionScrollHeight) * 140)
+    let sectionScrolledPercentY = Math.round( (currentScrollPosition / sectionScrollHeight) * 140)
+
+
+    const shapeToAnimate = document.getElementsByClassName("section-two__shape")[0];
+
+    shapeToAnimate.style.transform = `translate3d(${80 - sectionScrolledPercentX}%, ${80- sectionScrolledPercentY}%, 0) rotate(65deg)`;
+    shapeToAnimate.style.top = -sectionScrolledPercentY / 150 + '%';
+
+    if ( ( 80- sectionScrolledPercentX) <= -30 ) {
+      currentSectionContent.classList.remove('section-two__content--hidden')
+    } else {
+      currentSectionContent.classList.add('section-two__content--hidden')
+    }
+
+
+  } // end handleScroll
+
+
+})
